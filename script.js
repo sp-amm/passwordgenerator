542af4f61a68e1e7b8064c60300c26a1537989e7
sp <sam.pack.sp@gmail.com>
2020-08-30 11:57:22 +0930

    js file - changed the four arrays into a single array and made all characters strings

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//Arrays
+//Array for characters to be input to the password generator function
 
-const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
+const passwordCharacter = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", 
+                          //Numbers are 0-9 of the array
+                          "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+                          //Uppercase letters are 10-35 of the array
+                          "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+                          //Lowercase letters are 36-62
+                          "!", "@", "#", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "?", ",", "~",],
+                          //Special characters are 63-78
 
-const uppercase = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z,];
-
-const lowercase = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
-
-const special = ["!", "@", "#", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "?", ",", "~",],
 
 
+//Function to generate random number
+function getNumber() {
+  Math.floor(Math.random() * 
+}
 
 // Write password to the #password input
 function writePassword() {

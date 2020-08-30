114144b8a81eb8612f4b0fa9f7419b2d151c5a69
sp <sam.pack.sp@gmail.com>
2020-08-30 19:46:08 +0930

    added start of else if loop to exclude index numbers not chosen

--- script.js
+++ script.js
@@ -1,39 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
- 
-
-//Array for characters to be input to the password generator function
-
-const passwordCharacter = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", 
-                          //Numbers are 0-9 of the array
-                          "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-                          //Uppercase letters are 10-35 of the array
-                          "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-                          //Lowercase letters are 36-62
-                          "!", "@", "#", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "?", ",", "~",],
-                          //Special characters are 63-78
-
-
-
-//Function to loop through the array the numbers of times set by the  generate random characters from the array
-//function getChar() {
-  //for (var i = (Math.floor(Math.random() * 78); i < passwordLength; i++) {
-  //        result = passwordCharacter[];
- // }
-  // }
-}
-
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
-}
+  }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
@@ -42,8 +16,40 @@ generateBtn.addEventListener("click", writePassword);
 var passwordLength = prompt("How many characters do you want in your password?");
 console.log(passwordLength);
 
-var passwordUppercase = confirm("Do You want Upercase letters in your password?");
+var passwordUppercase = confirm("Do You want Uppercase letters in your password?");
 
 var passwordLowercase = confirm("Do you want lowercase letters in your password?");
 
-var passwordSpecial = confirm("Do you want special characters in your password?");
+var passwordSpecial = confirm("Do you want special characters in your password?"); 
+
+//Array for characters to be input to the password generator function
+
+const passwordCharacter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+                          //Uppercase letters are 0-9 of the array
+                          "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+                          //Lowercase letters are 10-35
+                          "!", "@", "#", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "?", ",", "~",
+                          //Special characters are 36-62
+                          "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",],
+                          //Numbers are 63-78 of the array
+
+//function to exclude index numbers that are excluded by preferences
+function indexNumber(){
+ if (passwordUppercase === true && passwordLowercase === true && passwordSpecial === true);
+    var prefNumber = false; 
+ 
+  else if (passwordUppercase === false && passwordLowercase === true && passwordSpecial === true);
+    var prefNumber = 10;
+ 
+  else if (passwordUppercase === false && passwordLowercase === false && passwordSpecial === true);
+    var prefNumber = 
+
+  else ()
+}
+
+//Function to loop through the array the numbers of times set by the  generate random characters from the array
+function generatePassword() {
+  for (var i = (Math.floor(Math.random() * 77) + prefNumber); i < password.Length; i++) {
+          result = passwordCharacter[];
+ // }
+  // }

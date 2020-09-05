47eed9733baa74fd46fe6deed579b8f3e2b654f7
sp <sam.pack.sp@gmail.com>
2020-09-05 21:06:37 +0930

    button is working and passwrod is generating in console log but not displaying and javascript contines to cycle through

--- script.js
+++ script.js
@@ -1,70 +1,75 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.getElementById("generate");
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var passwordLength = prompt ("How many characters in your password?");
-
-var numChar = confirm("Do you want numbers in your password?");
-
-var upperCase = confirm("Do You want Uppercase letters in your password?");
-
-var lowerCase = confirm("Do you want lowercase letters in your password?");
-
-var special = confirm("Do you want special characters in your password?");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.getElementById("password");
 
   passwordText.value = password;
   }
 
+ function generatePassword(){
+ 
+ //Collecting user preference data
+ let passwordLength = prompt("How many characters in your password length?");
+ let upper = confirm("Any uppercase?");
+ let lower = confirm("Any lowercase?");
+ let special = confirm("Any special characters?");
+ let number = confirm("Any numbers?");
+
+      
+ //Creating the array with the selected charaters
+ const passwordChar = [];
+
+ if (number === true) {
+  passwordChar.push("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+  console.log(passwordChar);
+ };
+
+ if (special === true) {
+   passwordChar.push("!", "@", "#", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "?", ",", "~");
+   console.log(passwordChar);
+ }
+
+ if (upper === true) {
+   passwordChar.push("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+   console.log(passwordChar);
+ }
+
+ if (lower === true) {
+   passwordChar.push("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+   console.log(passwordChar);
+ }
+
+ if (upper === false && lower === false && special === false && number === false) {
+   alert("You need to choose at least one type of character.");
+   password = 0;
+   console.log(pwrd);
+ }
+
+ 
+ //Generating random password from array
+   password = "";
+   for (var i=0; i <= passwordChar.length; i++) {       
+   password += passwordChar[Math.floor(Math.random()*passwordChar.length)];
+   }
 
 
+   console.log(password);
 
-//Array for characters to be input to the password generator function
-const passwordCharacter =  [
-                          "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-                          //Uppercase letters are 0-9 of the array
-                          "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-                          //Lowercase letters are 10-35
-                          "!", "@", "#", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "?", ",", "~",
-                          //Special characters are 36-62
-                          "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",]
-                            //Numbers are 63-78 of the array
-
-
-//function to exclude number and special character index numbers if ot selected
-
-//function generatePassword() {
-
-  for(let i = 0; i <= 8; i++) {
-          console.log(passwordCharacter[i]);
-            }
-        //Math.floor(Math.random() * 79)
-          //return passwordString;
+   return generatePassword(password);
+  
   };
- //if (numchar === true && upperCase=== true && === true){
-   // var prefNumber = ""
-    //} 
  
-  //else if (passwordUppercase === false && passwordLowercase === true && passwordSpecial === true)
-    //{var prefNumber = 10}
- 
-  //else if (passwordUppercase === false && passwordLowercase === false && passwordSpecial === true)
-    //{var prefNumber = 20}
-
-  //else ()
-//}
 
 
 
 
 
 
-
-  //return writePassword(){
-  //};
+  

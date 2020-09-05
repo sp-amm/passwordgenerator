b2c79eff4e24f6721aed742fea47758167ac63f2
sp <sam.pack.sp@gmail.com>
2020-09-05 11:58:39 +0930

    Figured out how to push to the an array using if condition to make sure the array only contains selected character types. (still in HTML doc not yet transferred to ..js file as nee to get button working)

--- script.js
+++ script.js
@@ -26,15 +26,15 @@ function writePassword() {
 
 
 //Array for characters to be input to the password generator function
-const passwordCharacter =  ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-                            //Numbers are 63-78 of the array
+const passwordCharacter =  [
                           "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
                           //Uppercase letters are 0-9 of the array
                           "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
                           //Lowercase letters are 10-35
-                          "!", "@", "#", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "?", ",", "~",],
+                          "!", "@", "#", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "?", ",", "~",
                           //Special characters are 36-62
-
+                          "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",]
+                            //Numbers are 63-78 of the array
 
 
 //function to exclude number and special character index numbers if ot selected
